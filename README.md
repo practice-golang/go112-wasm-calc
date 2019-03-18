4c7d96cd19c8ac7bbfda5243a16cae3ea1f5d401
Practice Golang <practice.golang@gmail.com>
2019-03-18 16:45:07 +0900

    서버 추가, calc.go 사칙연산 제대로 추가

---
+++ README.md
@@ -0,0 +1,1 @@
+Methods regarding `NewCallback` are deprecated in Go 1.12, Maybe.
--- calc.go
+++ calc.go
@@ -13,7 +13,65 @@ func add(this js.Value, i []js.Value) interface{} {
 	int1, _ := strconv.Atoi(value1)
 	int2, _ := strconv.Atoi(value2)
 
+	if value1 == "" {
+		int1 = 0
+	}
+	if value2 == "" {
+		int2 = 0
+	}
+
 	result := int1 + int2
+
+	return result
+}
+
+func sub(this js.Value, i []js.Value) interface{} {
+	value1 := i[0].String()
+	value2 := i[1].String()
+	int1, _ := strconv.Atoi(value1)
+	int2, _ := strconv.Atoi(value2)
+
+	if value1 == "" {
+		int1 = 0
+	}
+	if value2 == "" {
+		int2 = 0
+	}
+
+	result := int1 - int2
+
+	return result
+}
+
+func multi(this js.Value, i []js.Value) interface{} {
+	value1 := i[0].String()
+	value2 := i[1].String()
+	int1, _ := strconv.Atoi(value1)
+	int2, _ := strconv.Atoi(value2)
+
+	if value1 == "" {
+		int1 = 0
+	}
+	if value2 == "" {
+		int2 = 0
+	}
+
+	result := int1 * int2
+
+	return result
+}
+
+func divi(this js.Value, i []js.Value) interface{} {
+	value1 := i[0].String()
+	value2 := i[1].String()
+	int1, _ := strconv.Atoi(value1)
+	int2, _ := strconv.Atoi(value2)
+
+	if value2 == "" || value2 == "0" {
+		return "infinite"
+	}
+
+	result := int1 / int2
 
 	return result
 }
@@ -21,9 +79,11 @@ func add(this js.Value, i []js.Value) interface{} {
 func main() {
 	c := make(chan struct{}, 0)
 
-	println("WASM Go Initialized")
+	println("Go Web Assembly Ready")
 
 	global.Set("add", js.FuncOf(add))
-	global.Set("add2", js.FuncOf(add))
+	global.Set("sub", js.FuncOf(sub))
+	global.Set("multi", js.FuncOf(multi))
+	global.Set("divi", js.FuncOf(divi))
 	<-c
 }
